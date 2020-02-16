df1ac235147a76eb5bb890b48b1c13da1bbf4ecc
Yusuke Ogata <yusuke.ogata.0618.dev@gmail.com>
2020-02-16 17:24:46 +0900

    [add] lesson#12 正誤判定結果を選択肢に反映する
    正解した場合、選択肢が緑色に変化、不正解の場合、選択肢が赤色に変化させる

--- js/main.js
+++ js/main.js
@@ -46,9 +46,9 @@
    */
   function checkAnswer(li) {
     if (li.textContent === quizSet[currentNum].c[0]){
-      console.log("correct");
+      li.classList.add('correct');
     } else {
-      console.log("wrong");
+      li.classList.add('wrong');
     }
   }
   /**
